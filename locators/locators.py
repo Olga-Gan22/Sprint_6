b41b6bcee203abbfc67196fccc16da992e77fdd2
Olga Ganzhova <pelexotsa@yandex.ru>
2026-07-10 21:03:59 +0400

    fix: проверили и поправили локаторы для кнопок «Заказать» и выпадающих списков

--- locators/locators.py
+++ locators/locators.py
@@ -3,54 +3,47 @@ from selenium.webdriver.common.by import By
 class ScooterLocators:
     @staticmethod
     def question_button(index: int):
-        return (By.ID, f"accordion__heading-{index}")
+        return By.CSS_SELECTOR, f".accordion__item:nth-child({index + 1}) .accordion__button"
 
     @staticmethod
     def answer_panel(index: int):
-        return (By.ID, f"accordion__panel-{index}")
-
+        return By.CSS_SELECTOR, f".accordion__item:nth-child({index + 1}) .accordion__panel"
 
 class MainPageLocators:
-    BTN_ORDER_HEADER = ".Header_Nav__AGCXC button.Button_Button__ra12g"
-
-     # Кнопка «Заказать» внизу страницы (после скролла)
-    BTN_ORDER_FOOTER = ".Home_FinishButton__1_cWm button.Button_Button__ra12g"
-
-    # Логотип «Самокат» в шапке (ссылка)
-    LINK_LOGO = "a.Header_LogoScooter__3lsAR"
-
-    # Логотип Яндекса (target="_blank", ведёт наружу)
-    LINK_LOGO_YANDEX = "a.Header_LogoYandex__3TSOI"
+    BTN_ORDER_HEADER = (By.CSS_SELECTOR, ".Header_Nav__AGCXC button.Button_Button__ra12g")
+    BTN_ORDER_FOOTER = (By.CSS_SELECTOR, ".Home_FinishButton__1_cWm button.Button_Button__ra12g")
+    LINK_LOGO = (By.CSS_SELECTOR, "a.Header_LogoScooter__3lsAR")
+    LINK_LOGO_YANDEX = (By.CSS_SELECTOR, "a.Header_LogoYandex__3TSOI")
 
 
 class OrderPageLocators:
-    # --- Первая форма ---
-    INPUT_NAME = "input[placeholder='* Имя']"
-    INPUT_LAST_NAME = "input[placeholder='* Фамилия']"
-    INPUT_ADDRESS = "input[placeholder='* Адрес: куда привезти заказ']"
-    INPUT_METRO = ".select-search__input"
+    # Поля формы
+    INPUT_NAME = (By.CSS_SELECTOR, "input[placeholder='* Имя']")
+    INPUT_LAST_NAME = (By.CSS_SELECTOR, "input[placeholder='* Фамилия']")
+    INPUT_ADDRESS = (By.CSS_SELECTOR, "input[placeholder='* Адрес: куда привезти заказ']")
+    INPUT_METRO = (By.CSS_SELECTOR, "input.select-search__input")
     METRO_OPTION_BY_TEXT = "//*[contains(text(), '{metro_name}')]"
-    INPUT_PHONE = "input[placeholder='* Телефон: на него позвонит курьер']"
-    BTN_NEXT = ".Order_NextButton__1_rCA button.Button_Button__ra12g"
+    INPUT_PHONE = (By.CSS_SELECTOR, "input[placeholder='* Телефон: на него позвонит курьер']")
+    BTN_NEXT = (By.CSS_SELECTOR, ".Order_NextButton__1_rCA button.Button_Button__ra12g")
+
 
     # --- Вторая форма ---
-    INPUT_DATE = ".react-datepicker__input-container input"
-    DROPDOWN_RENT_PERIOD = ".Dropdown-control"
+    INPUT_DATE = (By.CSS_SELECTOR, ".react-datepicker__input-container input")
+    DROPDOWN_RENT_PERIOD = (By.CSS_SELECTOR, ".Dropdown-control")
+    RENT_PERIOD_OPTIONS_LIST = (By.CSS_SELECTOR, ".Dropdown-menu .Dropdown-option") 
+    CHECKBOX_BLACK = (By.CSS_SELECTOR, "#black")
+    CHECKBOX_GREY = (By.CSS_SELECTOR, "#grey")
+    INPUT_COMMENT = (By.CSS_SELECTOR, "input[placeholder='Комментарий для курьера']")
+    BTN_ORDER_FINAL = (By.CSS_SELECTOR, ".Order_Buttons__1xGrp button:not(.Button_Inverted__3IF-i)")
+    CALENDAR_POPPER = (By.CSS_SELECTOR, ".react-datepicker__popper")
+
     
-    # ВАЖНО: Ищем все видимые элементы внутри дропдауна (обычно это li или div)
-    RENT_PERIOD_OPTIONS_LIST = ".Dropdown-menu .Dropdown-option" 
+    # Модальные окна
+    BTN_MODAL_VIEW_STATUS = (By.CSS_SELECTOR, ".Order_Modal__YZ-d3 button.Button_Button__ra12g")
+    MODAL_WINDOW = (By.CSS_SELECTOR, ".Order_Modal__YZ-d3")
+    BTN_MODAL_YES = (By.CSS_SELECTOR, ".Order_Modal__YZ-d3 button:not(.Button_Inverted__3IF-i)")
+    BTN_MODAL_NO = (By.CSS_SELECTOR, ".Order_Modal__YZ-d3 button.Button_Inverted__3IF-i")
 
-    CHECKBOX_BLACK = "#black"
-    CHECKBOX_GREY = "#grey"
-    INPUT_COMMENT = "input[placeholder='Комментарий для курьера']"
-    BTN_ORDER_FINAL = ".Order_Buttons__1xGrp button:not(.Button_Inverted__3IF-i)"
+    
 
-
-    # Кнопка в модальном окне «Посмотреть статус»
-    BTN_MODAL_VIEW_STATUS = ".Order_Modal__YZ-d3 button.Button_Button__ra12g"
-    MODAL_WINDOW = ".Order_Modal__YZ-d3"
-
-    # --- Модальное окно ---
-    MODAL_WINDOW = ".Order_Modal__YZ-d3"
-    BTN_MODAL_YES = ".Order_Modal__YZ-d3 button:not(.Button_Inverted__3IF-i)"
-    BTN_MODAL_NO = ".Order_Modal__YZ-d3 button.Button_Inverted__3IF-i"
+    
